5ef0a7bd0a3bab4436af7628df514fcee5fb68f0
Juan Pablo Herrera <juanpablovelasco2@gmail.com>
2020-05-17 00:22:24 -0500

    Added Linear Regresion Algorithims and UI

---
+++ LinearRegresion/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace LinearRegresion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.RegresionChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.result = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.RegresionChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(173, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Regresion Lineal";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "PM10",
+            "CO2",
+            "NO2",
+            "O3"});
+            this.comboBox1.Location = new System.Drawing.Point(144, 114);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // RegresionChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.RegresionChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.RegresionChart.Legends.Add(legend1);
+            this.RegresionChart.Location = new System.Drawing.Point(62, 171);
+            this.RegresionChart.Name = "RegresionChart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.RegresionChart.Series.Add(series1);
+            this.RegresionChart.Size = new System.Drawing.Size(614, 371);
+            this.RegresionChart.TabIndex = 5;
+            this.RegresionChart.Text = "chart1";
+            this.RegresionChart.Click += new System.EventHandler(this.RegresionChart_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(526, 61);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 29);
+            this.result.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 591);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.RegresionChart);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.RegresionChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart RegresionChart;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ LinearRegresion/Form1.cs
@@ -0,0 +1,176 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinearRegresion
+{
+    public partial class Form1 : Form
+    {
+        List<Data> data = null;
+        List<String> Dates = new List<String>();
+        public static double Dslope;
+        public static double DYintercept;
+        double[] year = new double[] { 2011, 2012, 2013, 2014, 2015, 2016, 2017 };
+        double[] concentration;
+        public Form1()
+        {
+            InitializeComponent();
+            Dates.Add("09/04/2011 12:00:00 a. m.");
+            Dates.Add("09/04/2012 12:00:00 a. m.");
+            Dates.Add("09/04/2013 12:00:00 a. m.");
+            Dates.Add("09/04/2014 12:00:00 a. m.");
+            Dates.Add("09/04/2015 12:00:00 a. m.");
+            Dates.Add("09/04/2016 12:00:00 a. m.");
+            Dates.Add("09/04/2017 12:00:00 a. m.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String variable = "";
+            switch (comboBox1.SelectedItem)
+            {
+
+                case "PM10":
+                    variable = "PM10";
+                    break;
+
+                case "NO2":
+                    variable = "NO2";
+                    break;
+                case "O3":
+                    variable = "O3";
+                    break;
+
+            }
+            RegresionChart.Series.Clear();
+            RegresionChart.Series.Add("Data Points");
+            RegresionChart.Series["Data Points"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            concentration = new double[Dates.Count];
+            for (int r = 0; r < Dates.Count; r++)
+            {
+                callData("www.datos.gov.co", "ysq6-ri4e", Dates[r], variable);
+                double average = Average(data);
+                concentration[r] = average;
+
+                RegresionChart.Series["Data Points"].Points.AddXY(2011 + r, average);
+
+
+            }
+            RegresionChart.Series.Add("QR Line");
+            RegresionChart.Series["QR Line"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            RegresionChart.Series["QR Line"].Color = Color.Cyan;
+
+            double slope, yintercept, rSquared;
+            LinearRegression(year, concentration, out rSquared, out yintercept, out slope);
+            double predicted1 = (slope * year[0]) + yintercept;
+            double predicted2 = (slope * year[6]) + yintercept;
+            RegresionChart.Series["QR Line"].Points.AddXY(year[0], predicted1);
+            RegresionChart.Series["QR Line"].Points.AddXY(year[6], predicted2);
+            result.Text = "Pendiente : " + Math.Round(slope) + "\n" + "\n" + "Intercepto con Y : " + Math.Round(yintercept);
+
+        }
+
+
+        public void callData(String url, String id, String extension, String variable)
+        {
+            System.Net.HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://" + url + "/resource/" + id + ".json?$limit=" + 20 + "&fecha=" + extension + "&variable=" + variable);
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                var json = reader.ReadToEnd();
+                data = JsonConvert.DeserializeObject<List<Data>>(json);
+
+            }
+
+        }
+
+        public double Average(List<Data> data)
+        {
+            double acum = 0;
+            for (int c = 0; c < data.Count; c++)
+            {
+                acum += data[c].concentracion;
+            }
+            double average = acum / data.Count;
+            return average;
+        }
+
+        public static void LinearRegression(
+           double[] xVals,
+           double[] yVals,
+           out double rSquared,
+           out double yIntercept,
+           out double slope)
+        {
+            if (xVals.Length != yVals.Length)
+            {
+                throw new Exception("Input values should be with the same length.");
+            }
+
+            double sumOfX = 0;
+            double sumOfY = 0;
+            double sumOfXSq = 0;
+            double sumOfYSq = 0;
+            double sumCodeviates = 0;
+
+            for (var i = 0; i < xVals.Length; i++)
+            {
+                var x = xVals[i];
+                var y = yVals[i];
+                sumCodeviates += x * y;
+                sumOfX += x;
+                sumOfY += y;
+                sumOfXSq += x * x;
+                sumOfYSq += y * y;
+            }
+
+            var count = xVals.Length;
+            var ssX = sumOfXSq - ((sumOfX * sumOfX) / count);
+            var ssY = sumOfYSq - ((sumOfY * sumOfY) / count);
+
+            var rNumerator = (count * sumCodeviates) - (sumOfX * sumOfY);
+            var rDenom = (count * sumOfXSq - (sumOfX * sumOfX)) * (count * sumOfYSq - (sumOfY * sumOfY));
+            var sCo = sumCodeviates - ((sumOfX * sumOfY) / count);
+
+            var meanX = sumOfX / count;
+            var meanY = sumOfY / count;
+            var dblR = rNumerator / Math.Sqrt(rDenom);
+
+            rSquared = dblR * dblR;
+            yIntercept = meanY - ((sCo / ssX) * meanX);
+
+            slope = sCo / ssX;
+            Dslope = slope;
+            DYintercept = yIntercept;
+        }
+
+        public static double[] fillArray(List<Data> data, double[] array)
+        {
+            for (int c = 0; c < data.Count; c++)
+            {
+                array[c] = data[c].concentracion;
+            }
+            return array;
+        }
+
+        private void RegresionChart_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
